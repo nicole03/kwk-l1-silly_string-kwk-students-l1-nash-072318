86dfc58231b94c39e906d8e7356792ff9553ddc2
nicole03 <ntidwell03@gmail.com>
2018-07-23 18:48:17 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -3,11 +3,11 @@
 
 your_favorite_word = "spontaneous"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is spontaneous"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = your_favorite_word.upcase # Put a . after your_favorite word and use a String method.
 
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
